30256c1624bfdc99392a5be5f4aace83a6a95943
NShin <38845423+NTetora@users.noreply.github.com>
2025-05-02 01:20:26 +0900

    不要関数の削除

--- app/Services/TMDBService.php
+++ app/Services/TMDBService.php
@@ -15,26 +15,6 @@ class TMDBService
         $this->apiKey = config('services.tmdb.api_key');
     }
 
-    public function discoverMovies(array $options = []): array
-    {
-        $defaultOptions = [
-            'sort_by' => 'popularity.desc',
-            'year' => date('Y'),
-            'include_adult' => false,
-            'with_original_language' => 'ja', // 日本語映画をデフォルトで表示
-            'page' => 1, // 1ページ目
-        ];
-
-        $params = array_merge($defaultOptions, $options, [
-            'api_key' => $this->apiKey,
-            'language' => $this->language,
-        ]);
-
-        $response = Http::get("{$this->baseUrl}/discover/movie", $params);
-
-        return $response->json()['results'] ?? [];
-    }
-
     public function getPopularMovies(): array
     {
         $params = [
